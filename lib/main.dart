60af4c7f3ebe6e45a9b33b155dfc980cfb5abd21
Man Tran <tranduyminhman@gmail.com>
2024-03-21 10:35:48 +0700

    wip

--- lib/main.dart
+++ lib/main.dart
@@ -19,69 +19,93 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.indigo.shade600),
         useMaterial3: true,
       ),
-      home: const MyHomePage(title: 'Sám hối sáu căn'),
+      home: const MyHomePage(),
     );
   }
 }
 
 class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-  final String title;
+  const MyHomePage({super.key});
 
   @override
   State<MyHomePage> createState() => _MyHomePageState();
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  ScrollController scrollBookController = ScrollController();
   Future<String> loadAsset() async {
     return await rootBundle.loadString('assets/mds/samhoisaucan.md');
   }
 
-  final assetsAudioPlayer = AssetsAudioPlayer();
-  final audio = Audio("assets/audios/samhoisaucan1a.mp3");
+  late AssetsAudioPlayer _assetsAudioPlayer;
   var markdownData = '';
+
+  final audio = Audio("assets/audios/samhoisaucan1a.mp3");
 
   @override
   void initState() {
     super.initState();
-    assetsAudioPlayer.open(
+    loadBook();
+
+    _assetsAudioPlayer = AssetsAudioPlayer.newPlayer();
+    openPlayer();
+
+    Future.delayed(const Duration(seconds: 1), () {
+      scrollBookController.animateTo(
+          scrollBookController.position.maxScrollExtent,
+          duration: const Duration(seconds: 2),
+          curve: Curves.linear);
+    });
+  }
+
+  void openPlayer() async {
+    await _assetsAudioPlayer.open(
       audio,
       autoStart: false,
     );
-    loadAsset().then((value) => {
-          setState(() {
-            markdownData = value;
-          })
-        });
+  }
+
+  void loadBook() async {
+    var book = await loadAsset();
+    setState(() {
+      markdownData = book;
+    });
   }
 
   void _playOrPause() {
-    setState(() {
-      assetsAudioPlayer.playOrPause();
-    });
+    _assetsAudioPlayer.playOrPause();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+    _assetsAudioPlayer.dispose();
   }
 
   @override
   Widget build(BuildContext context) {
-    final bool playing = assetsAudioPlayer.isPlaying.value;
     return Scaffold(
-      body: SafeArea(
-          child: Markdown(
-        data: markdownData,
-        selectable: false,
-        styleSheet: MarkdownStyleSheet(
-            p: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
-            h1: const TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
-            h2: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
-            h3: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
-            h4: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)
-        ),
-      )),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _playOrPause,
-        tooltip: !playing ? 'Play' : 'Pause',
-        child: !playing ? const Icon(Icons.play_arrow) : const Icon(Icons.stop),
-      ),
-    );
+        body: SafeArea(
+            child: Markdown(
+          controller: scrollBookController,
+          data: markdownData,
+          selectable: false,
+          styleSheet: MarkdownStyleSheet(
+              p: const TextStyle(fontSize: 16.0, fontWeight: FontWeight.bold),
+              h1: const TextStyle(fontSize: 30.0, fontWeight: FontWeight.bold),
+              h2: const TextStyle(fontSize: 24.0, fontWeight: FontWeight.bold),
+              h3: const TextStyle(fontSize: 20.0, fontWeight: FontWeight.bold),
+              h4: const TextStyle(fontSize: 18.0, fontWeight: FontWeight.bold)),
+        )),
+        floatingActionButton:
+            _assetsAudioPlayer.builderIsPlaying(builder: (context, isPlaying) {
+          return FloatingActionButton(
+            onPressed: _playOrPause,
+            tooltip: !isPlaying ? 'Play' : 'Pause',
+            child: !isPlaying
+                ? const Icon(Icons.play_arrow)
+                : const Icon(Icons.stop),
+          );
+        }));
   }
 }
