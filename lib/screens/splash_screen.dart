3347f1bab3d9506eb8626fa2148398dcd1f4131d
Man Tran <tranduyminhman@gmail.com>
2024-03-26 14:20:17 +0700

    remove capu

--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -1,4 +1,3 @@
-import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:sam_hoi_sau_can/screens/home_screen.dart';
